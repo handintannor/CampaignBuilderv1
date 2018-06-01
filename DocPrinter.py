c8bcc3abc0f16ac0aeabd2a82de27fec42870299
Nathan <handintannor@gmail.com>
2018-05-31 20:35:18 -0600

    More reference files, I tried to start programming NPC's, but I realized I need a much more in depth plan for how to do it. Need to research more on program design.

---
+++ DocPrinter.py
@@ -0,0 +1,11 @@
+#This is meant to print the various generated information to a file.
+
+#For example, with races, I could give the command SaveRaces, and it would print them to a document.
+
+#I should add a LoadRace function as well
+
+#Functions:
+#   SaveRaces
+#   LoadRaces
+#   SaveClasses
+#   LoadClasses
---
+++ NPC Generator.py
@@ -0,0 +1,34 @@
+#This section of the code will generate a random NPC.
+#Types of NPC's:
+#Commoners
+#Full NPC's
+#Bosses
+
+#   Types of Common NPC's:
+#       Commoner    (farmers, townsfolk, cityfolk, merchants, sailors, etc.)
+#       Tradesman   (Craftsman, merchants, specialists, etc.)
+#       Warrior     (Town guards, merchant guards, bodyguards, thugs, etc.)
+#       Adept       (Shamans, Clerics, Wizards, etc.)
+#       Noble      (Nobles)
+
+#   Full NPC's:
+#       Normal PC Classes
+#       Normal PC Races
+#       Normal PC Backgrounds
+
+#   Bosses:
+#       Full NPC's
+#       Monsters
+#       Apply Templates
+#       Customize Spell Lists
+
+#   NPC Statistics
+#       Race
+#       Background
+#       Class
+#       Notable Traits
+#       Goals
+#       Political Background
+#       Voice
+#       Physicality
+#       Name
